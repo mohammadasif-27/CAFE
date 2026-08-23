bd802c14e3bded182f6e90f9ed68d4f166b4ee22
mohammadasif-27 <mohammad.asif2005a@gmail.com>
2026-08-23 08:41:37 +0530

    change the shpw btn in register page

--- core/views.py
+++ core/views.py
@@ -5,4 +5,13 @@ from menu.models import Food
 
 def home(request):
     foods = Food.objects.filter(available=True)
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+
+        print(name, email, phone, message)
+
     return render(request, "home.html", {"foods": foods})
