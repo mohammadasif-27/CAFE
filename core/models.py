b716faf5adf2990764fa5f0823bbc968f4760ad7
mohammadasif-27 <mohammad.asif2005a@gmail.com>
2026-08-29 23:34:54 +0530

    update core model

--- core/models.py
+++ core/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from menu.models import Food
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.IntegerField(default=5)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    rating = models.IntegerField()
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user.username} - {self.food.name}"
