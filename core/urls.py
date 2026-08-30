c1b0e6e1e4a464c696bc9b693e550ca208735b81
mohammadasif-27 <mohammad.asif2005a@gmail.com>
2026-08-30 13:09:48 +0530

    add review system for  every foods

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", views.home, name="home"),
+    path("add-review/", views.add_review, name="add_review"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,24 +1,31 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Review
 from menu.models import Food
+
 
 def home(request):
     foods = Food.objects.filter(available=True)
     reviews = Review.objects.all().order_by("-created_at")
 
-    if request.method == "POST":
-
-        if request.user.is_authenticated:
-
-            Review.objects.create(
-                user=request.user,
-                rating=request.POST.get("rating"),
-                message=request.POST.get("review"),
-            )
-
-        return redirect("home")
-
     return render(request, "home.html", {
         "foods": foods,
         "reviews": reviews,
     })
+
+
+def add_review(request):
+    if request.method == "POST" and request.user.is_authenticated:
+
+        food = get_object_or_404(
+            Food,
+            id=request.POST.get("food_id")
+        )
+
+        Review.objects.create(
+            user=request.user,
+            food=food,
+            rating=request.POST.get("rating"),
+            message=request.POST.get("review"),
+        )
+
+    return redirect("home")
